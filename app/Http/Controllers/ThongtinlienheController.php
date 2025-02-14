8bdefcd3943e0c4c67347e2a2c71e21d82a730a5
datprs <datprs@gmail.com>
2025-02-14 10:37:26 +0700

    Change all to storage

--- app/Http/Controllers/ThongtinlienheController.php
+++ app/Http/Controllers/ThongtinlienheController.php
@@ -4,20 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Thongtinlienhe;
+
 class ThongtinlienheController extends Controller
 {
     public function index()
     {
-    	$thongtinlienhe = Thongtinlienhe::paginate(15);
-    	return view('admin.thongtinlienhe.index',[
+        $thongtinlienhe = Thongtinlienhe::paginate(15);
+        return view('admin.thongtinlienhe.index', [
             'thongtinlienhe' => $thongtinlienhe,
         ]);
     }
 
     public function update($id)
     {
-    	$thongtinlienhe = Thongtinlienhe::find($id);
-    	return view('admin.thongtinlienhe.update',[
+        $thongtinlienhe = Thongtinlienhe::find($id);
+        return view('admin.thongtinlienhe.update', [
             'thongtinlienhe' => $thongtinlienhe,
         ]);
     }
@@ -25,7 +26,7 @@ class ThongtinlienheController extends Controller
     public function updatePost(Request $request, $id)
     {
         // dd($request->all());
-    	$thongtinlienhe = Thongtinlienhe::find($id);
+        $thongtinlienhe = Thongtinlienhe::find($id);
 
         $thongtinlienhe->fill([
             'hoten' => $request->hoten,
@@ -45,20 +46,21 @@ class ThongtinlienheController extends Controller
 
         $thongtinlienhe->save();
 
-    	return redirect('admin/thongtinlienhe/index')->with('thongbao', 'Sửa thành công !');
+        return redirect('admin/thongtinlienhe/index')->with('thongbao', 'Sửa thành công !');
     }
 
     public function delete($id)
     {
-    	$thongtinlienhe = Thongtinlienhe::find($id);
-    	$thongtinlienhe->delete();
-    	return redirect('admin/thongtinlienhe/index')->with('thongbao', 'Bạn đã xóa thành công !');
+        $thongtinlienhe = Thongtinlienhe::find($id);
+        $thongtinlienhe->delete();
+        return redirect('admin/thongtinlienhe/index')->with('thongbao', 'Bạn đã xóa thành công !');
     }
 
     // Tìm kiếm ajax
-    public function ajaxFilter(Request $request){
+    public function ajaxFilter(Request $request)
+    {
         $key_word = $request->get('key_word', '');
-        return Thongtinlienhe::where('name', 'like', '%'.$key_word.'%')
-                                ->get();
+        return Thongtinlienhe::where('name', 'like', '%' . $key_word . '%')
+            ->get();
     }
 }
